9ecdc7b2482ce919dcfc5b40457fb1f62cb66815
Jason <46291780+JasonMorta@users.noreply.github.com>
2024-07-19 12:19:44 +0200

    updates

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,14 +1,28 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {
     public: { url: '/', static: true },
-    src: { url: '/dist' }
+    src: { url: '/dist' },
   },
   plugins: [
-     /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
     '@snowpack/plugin-react-refresh',
-    '@snowpack/plugin-typescript',
+    '@snowpack/plugin-dotenv',
+    [
+      '@snowpack/plugin-typescript',
+      {
+        /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
+        ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
+      },
+    ],
   ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
   packageOptions: {
     /* ... */
   },
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.FC = () => {
+interface AppProps {}
+
+function App({}: AppProps) {
+  // Create the count state.
+  const [count, setCount] = useState(0);
+  // Create the counter (+1 every second).
+  useEffect(() => {
+    const timer = setTimeout(() => setCount(count + 1), 1000);
+    return () => clearTimeout(timer);
+  }, [count, setCount]);
+  // Return the App component.
   return (
-    <div>
-      <h1>Hello, Snowpack with React and TypeScript!</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Snowpack React ts base code
+        </p>
+        <p>
+          Page has been open for <code>{count}</code> seconds.
+        </p>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
